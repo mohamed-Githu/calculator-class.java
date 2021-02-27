c4ad3eb6de153f9bf3d0559bc81f180840d77905
Mohamed Hussein <mohamed.hussein151515@gmail.com>
2021-02-27 22:37:08 +0200

    first commit

---
+++ src/calcclass/Calc.java
@@ -0,0 +1,8 @@
+package calcclass;
+
+public class Calc {
+    public static double sum(double a,double b) { return a + b; }
+    public static double sub(double a,double b) { return a - b; }
+    public static double mult(double a,double b) { return a * b; }
+    public static double div(double a,double b) { return a / b; }
+}
---
+++ src/calcclass/CalcClass.java
@@ -0,0 +1,15 @@
+package calcclass;
+
+public class CalcClass {
+
+    public static void main(String[] args) {
+//        System.out.println(Calc.mult(5, 9));
+//        System.out.println(Calc.sub(5, 9));
+//        System.out.println(Calc.sum(5, 9));
+//        System.out.println(Calc.div(5, 9));
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+    }
+    
+}
